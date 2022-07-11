f055f2df69f25cf8664e485727d7a2391a721df9
fabrizioguespe <fabrizioguespe@MacFabri.local>
2022-07-11 15:30:39 -0300

    ABI folder optimized

--- examples/aave.js
+++ examples/aave.js
@@ -1,5 +1,8 @@
 
 const Web3 = require("web3");
+const lendingPoolABI = require("../abi/Pool.json")
+const ERC20_ABI =require("../abi/ERC20.json")
+
 //Providers
 const web3 = new Web3(new Web3.providers.HttpProvider('https://api.avax-test.network/ext/bc/C/rpc'))
 
@@ -7,9 +10,6 @@ const web3 = new Web3(new Web3.providers.HttpProvider('https://api.avax-test.net
 const privateKey1 = '67633be8c32db5414951db4a9ea9734b1214f8f5ca15d6b16818c0b4ee864653' // Private key of account 1
 const signer = web3.eth.accounts.privateKeyToAccount(privateKey1)
 web3.eth.accounts.wallet.add(signer);
-
-const lendingPoolABI = require("../abi/Pool.json")
-const ERC20_ABI =[{"constant":true,"inputs":[],"name":"name","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_spender","type":"address"},{"name":"_value","type":"uint256"}],"name":"approve","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"totalSupply","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_from","type":"address"},{"name":"_to","type":"address"},{"name":"_value","type":"uint256"}],"name":"transferFrom","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"decimals","outputs":[{"name":"","type":"uint8"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"_owner","type":"address"}],"name":"balanceOf","outputs":[{"name":"balance","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"symbol","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_to","type":"address"},{"name":"_value","type":"uint256"}],"name":"transfer","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"_owner","type":"address"},{"name":"_spender","type":"address"}],"name":"allowance","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"payable":true,"stateMutability":"payable","type":"fallback"},{"anonymous":false,"inputs":[{"indexed":true,"name":"owner","type":"address"},{"indexed":true,"name":"spender","type":"address"},{"indexed":false,"name":"value","type":"uint256"}],"name":"Approval","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"from","type":"address"},{"indexed":true,"name":"to","type":"address"},{"indexed":false,"name":"value","type":"uint256"}],"name":"Transfer","type":"event"}]
 
 //Contracts
 const pool_contract_addr='0xb47673b7a73D78743AFF1487AF69dBB5763F00cA'
